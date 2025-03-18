87e3053efce365087d827354595ee9c952e5ad83
FrankieFosse <146066047+FrankieFosse@users.noreply.github.com>
2025-03-18 13:13:25 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import { PiHouseSimple } from "react-icons/pi";
 import { PiPlusCircle } from "react-icons/pi";
+import { PiUserLight } from "react-icons/pi";
 
 function Header() {
 
   return (
     <header className="bg-BGcolor">
-      <nav className="flex flex-row w-full h-16 justify-between items-center px-4">
+      <nav className="flex flex-row w-full h-16 justify-between items-center px-4 text-2xl">
         <PiHouseSimple />
         <PiPlusCircle />
-        <h1 className="text-lg">Profile</h1>
+        <PiUserLight />
       </nav>
     </header>
   )
