e3af60ab0e6a7f353fc42a855e69825a997e98f1
FrankieFosse <146066047+FrankieFosse@users.noreply.github.com>
2025-03-18 14:22:34 +0100

    Changes to router, header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link, useLocation } from 'react-router'
 import { PiHouseSimple } from "react-icons/pi";
 import { PiPlusCircle } from "react-icons/pi";
 import { PiUserLight } from "react-icons/pi";
@@ -5,11 +6,11 @@ import { PiUserLight } from "react-icons/pi";
 function Header() {
 
   return (
-    <header className="bg-BGcolor">
+    <header className="bg-navbar fixed bottom-0 w-full opacity-90">
       <nav className="flex flex-row w-full px-10 h-16 justify-between items-center px-4 text-2xl">
-        <PiHouseSimple className="hover:scale-110 transition duration-150" />
-        <PiPlusCircle className="hover:scale-110 transition duration-150" />
-        <PiUserLight className="hover:scale-110 transition duration-150" />
+        <Link to="/"><PiHouseSimple className="hover:scale-110 transition duration-150" /></Link>
+        <Link to="/create"><PiPlusCircle className="hover:scale-110 transition duration-150" /></Link>
+        <Link to="/profile"><PiUserLight className="hover:scale-110 transition duration-150" /></Link>
       </nav>
     </header>
   )
