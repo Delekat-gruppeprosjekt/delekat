42469667dd6db322993e1276b9e56f59911bf8a7
FrankieFosse <146066047+FrankieFosse@users.noreply.github.com>
2025-03-19 14:21:41 +0100

    Changes to Header, navbar, Layout margin

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import Header from '../components/Header'
 const Layout = () => {
     return (
         <div>
-            <main>
+            <main className="xl:ml-100">
                 <Outlet />
             </main>
             <Header />
--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,18 @@ import App from "./App";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/clerk-react";
 
-const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+/* const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!clerkPublishableKey) {
   throw new Error(
     "Missing Clerk publishable key. Please add it to your .env file."
   );
-}
+} */
 
 createRoot(document.getElementById("root")).render(
-  <ClerkProvider publishableKey={clerkPublishableKey}>
+  //<ClerkProvider publishableKey={clerkPublishableKey}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </ClerkProvider>
+  //</ClerkProvider>
 );
