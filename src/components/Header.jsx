08817e85efae03f6d5219c54720b5fd0030168c6
FrankieFosse <146066047+FrankieFosse@users.noreply.github.com>
2025-03-20 15:16:05 +0100

    Update constants, navbar update and added Search function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import { PiHouseSimple } from "react-icons/pi";
 import { PiPlusCircle } from "react-icons/pi";
 import { PiUserLight } from "react-icons/pi";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function Header() {
   const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 1280);
+  const location = useLocation();
+  const [activePath, setActivePath] = useState(location.pathname);
 
   useEffect(() => {
     const handleResize = () => setIsLargeScreen(window.innerWidth > 1280);
@@ -13,18 +15,22 @@ function Header() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  useEffect(() => {
+    setActivePath(location.pathname);
+  }, [location.pathname]);
+
   return (
     <header className="bg-navbar fixed bottom-0 w-full opacity-90 border-navbar-border border-t-1
                         xl:left-0 xl:h-full xl:w-100 xl:border-r-1 xl:border-t-0 xl:opacity-100">
       <nav className="flex flex-row w-full px-10 h-16 justify-between items-left text-2xl
                         xl:left-0 xl:flex-col xl:h-1/5 xl:justify-evenly xl:text-lg">
-        <Link to="/" className="flex items-center gap-2 hover:scale-110 transition duration-150 w-max">
+        <Link to="/" className={`flex items-center gap-2 hover:scale-110 transition duration-150 w-max ${activePath === "/" ? "text-PMgreen" : ""}`}>
           <PiHouseSimple /> {isLargeScreen ? "Hjem" : ""}
         </Link>
-        <Link to="/create" className="flex items-center gap-2 hover:scale-110 transition duration-150 w-max">
+        <Link to="/create" className={`flex items-center gap-2 hover:scale-110 transition duration-150 w-max ${activePath === "/create" ? "text-PMgreen" : ""}`}>
           <PiPlusCircle /> {isLargeScreen ? "Post ny oppskrift" : ""}
         </Link>
-        <Link to="/profile" className="flex items-center gap-2 hover:scale-110 transition duration-150 w-max">
+        <Link to="/profile" className={`flex items-center gap-2 hover:scale-110 transition duration-150 w-max ${activePath === "/profile" ? "text-PMgreen" : ""}`}>
           <PiUserLight /> {isLargeScreen ? "Profil" : ""}
         </Link>
       </nav>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import HomeList from "../../components/home/HomeList.jsx";
 import { PiMagnifyingGlass } from "react-icons/pi";
 
-
-
 export default function Home() {
+  const [showSearch, setShowSearch] = useState(false);
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (event) => {
+    const query = event.target.value.toLowerCase();
+    setSearchQuery(query);
+  };
+
+
   return (
     <div className="min-h-screen p-6 bg-[var(--color-BGcolor)]">
       <div className="flex items-center justify-start ml-1">
         <img
           src="/assets/DelekatLogo.svg"
           alt="Delekat Logo"
-          
-        loading="lazy"
-        className="h-auto w-22"
+          loading="lazy"
+          className="h-auto w-22"
         />
       </div>
       <h1 className="text-3xl font-thin mb-6 flex justify-center mt-8">
         La deg friste
       </h1>
-      <div className="absolute right-0 top-0 m-8 text-2xl hover:scale-110 duration-150 cursor-pointer"><PiMagnifyingGlass /></div>
-      <HomeList />
+      <div 
+        className="absolute right-0 top-0 m-8 text-2xl hover:scale-110 duration-150 cursor-pointer"
+        onClick={() => setShowSearch(!showSearch)}
+      >
+        <PiMagnifyingGlass />
+      </div>
+      {showSearch && (
+        <div className="flex justify-center">
+          <input 
+            className="bg-navbar w-2/4 p-2 rounded-lg mb-6 outline-0" 
+            type="text"
+            value={searchQuery}
+            onChange={handleSearch}
+            placeholder="Søk"
+          />
+        </div>
+      )}
+      <HomeList searchQuery={searchQuery} />
     </div>
   );
 }
