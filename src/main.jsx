6136c3c209698872d6b1af693e009989e92174bd
FrankieFosse <146066047+FrankieFosse@users.noreply.github.com>
2025-03-26 12:24:29 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/clerk-react";
 import { AuthProvider } from "./contexts/authContext/auth";
 
-const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!clerkPublishableKey) {
-  throw new Error(
-    "Missing Clerk publishable key. Please add it to your .env file."
-  );
-} 
-
 createRoot(document.getElementById("root")).render(
-  <ClerkProvider publishableKey={clerkPublishableKey}>
     <React.StrictMode>
       <AuthProvider>
         <App />
       </AuthProvider>
     </React.StrictMode>
-  </ClerkProvider>
 );
