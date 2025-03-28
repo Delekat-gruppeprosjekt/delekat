2ae6be9920ddf021bf21e443d0f152977af0386d
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2025-03-28 15:15:50 +0100

    klar.

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,12 @@
-import LoginComponent from "../../components/login/login.jsx"
+import LoginComponent from "../../components/login/login.jsx";
 
 const Login = () => {
   return (
-    <div className="min-h-screen p-6 bg-BGcolor">
-      <h1>Login Page</h1>
-      <LoginComponent />
+    <div className="min-h-screen bg-BGcolor flex items-center justify-center p-6">
+      <div className="max-w-[1400px] mx-auto px-4 w-full">
+        <h1 className="text-center text-3xl font-bold mb-6">Login Page</h1>
+        <LoginComponent />
+      </div>
     </div>
   );
 };
